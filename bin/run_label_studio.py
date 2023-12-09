4946076bf8e53d3fda3c225f482af081ccc44611
r1cheu <chenrulei@cemps.ac.cn>
2023-12-09 15:21:03 +0800

    add label studio files

---
+++ bin/run_label_studio.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import os
+import subprocess
+
+host_ip = os.environ.get('host_ip')
+sam_weight = os.environ.get('sam_weight')
+label_anything = os.environ.get('label_anything')
+
+cmd = ('nohup label-studio-ml start sam --port 8005 --with sam_config=vit_b '
+       f'sam_checkpoint_file={sam_weight} out_bbox=True '
+       'out_mask=False device=cuda:7 model_name=sam_hq >/dev/null 2>&1 &')
+
+cmd2 = (f'nohup label-studio start --internal-host {host_ip} --port 8080'
+        ' --no-browser >/dev/null 2>&1 &')
+
+if __name__ == '__main__':
+    print('Starting Label Studio...')
+    print(cmd, '\n', cmd2)
+    os.chdir(label_anything)
+    subprocess.run(cmd, shell=True)
+    subprocess.run(cmd2, shell=True)
