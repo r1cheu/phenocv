e73485253aebd04ba36b82a11d7ed977b36855d9
RL Chen <chenrulei@cemps.ac.cn>
2024-01-31 17:29:30 +0800

    bump version to 0.1.1

--- phenocv/__init__.py
+++ phenocv/__init__.py
@@ -2,6 +2,6 @@ from .registry import registry
 from confection import Config
 from phenocv.analysis import PanicleAnalyzer
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 __all__ = ['registry', 'Config', 'PanicleAnalyzer']
